40e82c998f6447cfae488a2eec2209376ee3ac18
n.Caio8 <75146855+codenamecaio@users.noreply.github.com>
2025-10-04 18:50:11 +0000

    adicionado

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       "Por isso eu afirmo a vocês: quando vocês orarem e pedirem alguma coisa,
-      creiam que já a receberam, e assim tudo lhes será dado. " Marcos 11:24
+      creiam que já a receberam, e assim tudo lhes será dado... " Marcos 11:24
       (NTLH)
     </h1>
   );
